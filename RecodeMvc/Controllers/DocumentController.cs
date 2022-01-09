693eb7a77a1bcb8374d6ad51317aa5863b620eae
Lucas Fernandes <lucasfernandes_222@hotmail.com>
2022-01-08 21:21:09 -0300

    Final Commit

---
+++ RecodeMvc/Controllers/DocumentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecodeMvc.Controllers
+{
+    public class DocumentController : Controller
+    {
+        public IActionResult Document()
+        {
+            return View();
+        }
+        public IActionResult Job()
+        {
+            return View();
+        }
+        public IActionResult Map()
+        {
+            return View();
+        }
+            public IActionResult Emespanhol()
+            {
+                return View();
+            }
+        public IActionResult Duvidas()
+        {
+            return View();
+        }
+    }
+    }
+
---
+++ RecodeMvc/Models/UsuarioContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecodeMvc.Models
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext()
+        {
+
+        }
+
+        public UsuarioContext(DbContextOptions<UsuarioContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
